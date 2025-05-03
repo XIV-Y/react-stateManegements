0e15badb2b640ac2a09d5d4a403c208fbf61837b
XIV-Y <157947753+XIV-Y@users.noreply.github.com>
2025-05-03 22:52:36 +0900

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("Failed to find the root element");
+}
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
